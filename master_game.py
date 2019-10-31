e989acae1ac6aa19bdee19f74a1c3fe1a664d241
SwuntiiTHICC <ZHENGSTA7@gmail.com>
2019-10-31 11:10:08 -0400

    coles changes onto my branch

--- master_game.py
+++ master_game.py
@@ -1,17 +1,54 @@
-import pygame
+from enum import Enum
+import pygame as pg
+
+class Screen(Enum):
+    STARTING = 0
+    HOME = 1
+
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode([500,500])
-    screen.fill([25, 226, 230])
-    #running state of the game variable
-    run = True
-    while run:
-        #event handling and visuals go in here
-        for event in pygame.event.get():
-            #can do other stuff in here for listeners
-            if event.type == pygame.QUIT:
-                run = False
 
-if __name__ == "__main__":
-    main()
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255) # More colours should be added here
+    WIDTH = 800
+    HEIGHT = 480
+
+    pg.init()
+    pg.font.init()
+    titleFont = pg.font.SysFont('VT323-Regular.ttf', 180)
+    textFont = pg.font.SysFont('VT323-Regular.ttf', 100)
+    
+    screen = pg.display.set_mode((WIDTH, HEIGHT), 0, 32)
+
+    currGameState = Screen.STARTING
+
+    while True:
+        ev = pg.event.get()
+        screen.fill(WHITE)
+        if currGameState == Screen.STARTING:
+            outerRect = pg.Rect(WIDTH / 2, HEIGHT / 2, 410, 160)
+            innerRect = pg.Rect(WIDTH / 2, HEIGHT / 2, 390, 140)
+            outerRect.centerx = WIDTH / 2 #draw rectangles at the center of the screen
+            outerRect.centery = HEIGHT / 2
+            innerRect.center = outerRect.center
+            pg.draw.rect(screen, BLACK, outerRect)
+            pg.draw.rect(screen, WHITE, innerRect)
+
+            title = titleFont.render('JikoAi', True, (0, 0, 0))
+            screen.blit(title,(WIDTH / 4 + 13, HEIGHT / 2 - 57))
+            if pg.mouse.get_pressed()[0]:
+                currGameState = Screen.HOME
+
+        elif currGameState == Screen.HOME:
+            welcome = textFont.render('h', True, (0, 0, 0))
+            screen.blit(welcome,(WIDTH / 4 + 13, HEIGHT / 2 - 57))
+        
+        for event in ev:
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+        pg.display.update()
+
+
+main()
