96446f8b07a7904c4dbaf75bc27c92517bd29153
simcard0000 <43153413+simcard0000@users.noreply.github.com>
2019-10-31 16:40:37 -0400

    Working code for requests and input
    A lot of things are happening!

--- audioRequestIBM.py
+++ audioRequestIBM.py
@@ -1,14 +1,14 @@
-import requests
+#request section for the audio processing
 
 url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
 
 headers = {
     'Content-Type': "audio/flac",
-    'Authorization': "Basic -",
+    'Authorization': "Basic YXBpa2V5OmswOV9hV0I3Rmxma3hrS050TjdaQjhwUUdqMTI2ZTRJUlJwNVJ4VTNGYWwz",
     'User-Agent': "PostmanRuntime/7.19.0",
     'Accept': "*/*",
     'Cache-Control': "no-cache",
-    'Postman-Token': "-",
+    'Postman-Token': "cd590707-f657-4057-9c69-b326f568ae1d,44ad2c13-0c6b-4bbb-943f-539869afe4d8",
     'Host': "stream.watsonplatform.net",
     'Accept-Encoding': "gzip, deflate",
     'Content-Length': "285928",
@@ -16,6 +16,10 @@ headers = {
     'cache-control': "no-cache"
     }
 
-response = requests.request("POST", url, headers=headers)
+data = open('C:\\Users\\Sims\\Downloads\\IBMWatsonTest.flac', 'rb')
+
+response = requests.post(url, data, headers=headers)
 
 print(response.text)
+
+#what to do with the responses and stuff
---
+++ microphoneInput.py
@@ -0,0 +1,37 @@
+import wave
+import requests
+import pyaudio
+
+#getting audio for stuff
+
+chunk = 8192
+sample_format = pyaudio.paInt16
+channels = 2
+fs = 44100
+seconds = 10
+filename = 'command.wav'
+p = pyaudio.PyAudio()
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+print("starting recording")
+frames = []
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+print("stopping recording")
+filewriting = wave.open(filename, 'wb')
+filewriting.setnchannels(channels)
+filewriting.setsampwidth(p.get_sample_size(sample_format))
+filewriting.setframerate(fs)
+filewriting.writeframes(b''.join(frames))
+filewriting.close()
