220d221ec81f9a2844dc37c1ff2c659d0089dbde
simcard0000 <43153413+simcard0000@users.noreply.github.com>
2019-11-01 19:16:29 -0400

    Getting stuff working again
    Putting audio input and requests together

--- audioRequestIBM.py
+++ audioRequestIBM.py
@@ -1,15 +1,14 @@
 import requests
-#request section for the audio processing
 
 url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
 
 headers = {
     'Content-Type': "audio/flac",
-    'Authorization': "Basic YXBpa2V5OmswOV9hV0I3Rmxma3hrS050TjdaQjhwUUdqMTI2ZTRJUlJwNVJ4VTNGYWwz",
+    'Authorization': "Basic YXBpa2V5OjY2Ylk2SmljTUh0LW1SdjJCZk8tYzEwLTJ0U0xweVVTZkRRTlFjU3A5WV90",
     'User-Agent': "PostmanRuntime/7.19.0",
     'Accept': "*/*",
     'Cache-Control': "no-cache",
-    'Postman-Token': "cd590707-f657-4057-9c69-b326f568ae1d,44ad2c13-0c6b-4bbb-943f-539869afe4d8",
+    'Postman-Token': "507a31da-ffc7-430e-9dad-b8006da32933,270b9439-797f-4e71-bc99-c13feb217166",
     'Host': "stream.watsonplatform.net",
     'Accept-Encoding': "gzip, deflate",
     'Content-Length': "285928",
@@ -22,5 +21,3 @@ data = open('C:\\Users\\Sims\\Downloads\\IBMWatsonTest.flac', 'rb')
 response = requests.post(url, data, headers=headers)
 
 print(response.text)
-
-#what to do with the responses and stuff
--- microphoneInput.py
+++ microphoneInput.py
@@ -6,6 +6,7 @@ import pyaudio
 
 chunk = 8192
 sample_format = pyaudio.paInt16
+#two channels for Windows, one channel for Mac
 channels = 2
 fs = 44100
 seconds = 10
@@ -38,3 +39,25 @@ filewriting.setsampwidth(p.get_sample_size(sample_format))
 filewriting.setframerate(fs)
 filewriting.writeframes(b''.join(frames))
 filewriting.close()
+
+url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
+
+headers = {
+    'Content-Type': "audio/wav",
+    'Authorization': "Basic YXBpa2V5OjY2Ylk2SmljTUh0LW1SdjJCZk8tYzEwLTJ0U0xweVVTZkRRTlFjU3A5WV90",
+    'User-Agent': "PostmanRuntime/7.19.0",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "507a31da-ffc7-430e-9dad-b8006da32933,270b9439-797f-4e71-bc99-c13feb217166",
+    'Host': "stream.watsonplatform.net",
+    'Accept-Encoding': "gzip, deflate",
+    'Content-Length': "285928",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+data = open('command.wav', 'rb')
+
+response = requests.post(url, data, headers=headers)
+
+print(response.text)
