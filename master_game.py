b610e3401658c916c453677d9cf0af54c92d8e19
wenyihu3 <56973754+wenyihu3@users.noreply.github.com>
2019-11-07 11:03:34 -0500

    Add files via upload

---
+++ master_game.py
@@ -0,0 +1,87 @@
+from enum import Enum
+import pygame as pg
+import sys 
+from Button import Button
+
+class Screen(Enum):
+    STARTING = 0
+    HOME = 1
+    EGG = 2
+    Q_A = 3
+    HATCH = 4
+    FOOD = 5
+    WATER = 6
+    FUN = 7
+
+
+def main():
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255) # More colours should be added here
+    WIDTH = 800
+    HEIGHT = 480
+
+    pg.init()
+    pg.font.init()
+    # titleFont = pg.font.SysFont('VT323-Regular.ttf', 180)
+    # textFont = pg.font.SysFont('VT323-Regular.ttf', 100)
+    
+    screen = pg.display.set_mode((WIDTH, HEIGHT), 0, 32)
+
+    # titleBG = GIFImage("graphicAssets/BgTitle2.gif")
+    # titleBG = pg.transform.scale(titleBG.getImage, (1280, 720))
+    
+    currGameState = Screen.STARTING
+
+    while True:
+        ev = pg.event.get()
+        screen.fill(WHITE)
+
+        if currGameState == Screen.STARTING:
+
+            outerRect = pg.Rect(WIDTH / 2, HEIGHT / 2, 410, 160)
+            innerRect = pg.Rect(WIDTH / 2, HEIGHT / 2, 390, 140)
+            outerRect.centerx = WIDTH / 2 #draw rectangles at the center of the screen
+            outerRect.centery = HEIGHT / 2
+            innerRect.center = outerRect.center
+            pg.draw.rect(screen, BLACK, outerRect)
+            pg.draw.rect(screen, WHITE, innerRect)
+
+            homeButton = Button(screen, 200, 200, 100, 100, function = 0, color = (233, 255, 255), hover_color = (454, 255, 255), border= True, border_width = 2, border_colour = (0, 0, 0), txt= True, text ='dfdf', font_name = 'arial', text_size = 20, text_color = (0, 0, 0), check_bold= True)
+            homeButton.draw()
+            homeButton.mouse_hovering()
+            homeButton.update()
+            homeButton.show_text()
+            homeButton.click()
+
+            # title = titleFont.render('JikoAi', True, (0, 0, 0))
+            # screen.blit(title,(WIDTH / 4 + 13, HEIGHT / 2 - 57))
+
+            # titleBG.render(screen, (0, 0))
+            pg.display.update()
+
+        elif currGameState == Screen.HOME:
+            welcome = textFont.render('h', True, (0, 0, 0))
+            screen.blit(welcome,(WIDTH / 4 + 13, HEIGHT / 2 - 57))
+        
+        elif currGameState == Screen.EGG:
+            print("FILLER")
+        elif currGameState == Screen.FOOD:
+            print("FILLER")
+        elif currGameState == Screen.HATCH:
+            print("FILLER")
+        elif currGameState == Screen.Q_A:
+            print("FILLER")
+        elif currGameState == Screen.WATER:
+            print("FILLER")
+        elif currGameState == Screen.FUN:
+            print("FILLER")
+            
+        for event in ev:
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+
+main()
